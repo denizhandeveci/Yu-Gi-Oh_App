9b9ff070b9718ee7e161c1509cd177891f53ca5d
denizhandeveci <devecidenizhanyigit@gmail.com>
2025-03-06 15:10:03 -0600

    asdasd

---
+++ src/main/java/com/Yu_Gi_Oh_App/duel_simulator/DuelSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.Yu_Gi_Oh_App.duel_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Yu_Gi_Oh_App/duel_simulator/DuelSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yu_Gi_Oh_App.duel_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuelSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
