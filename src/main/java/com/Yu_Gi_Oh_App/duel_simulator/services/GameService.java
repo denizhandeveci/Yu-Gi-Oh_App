972216944ca137e7a065433fc133773d3b37c960
denizhandeveci <devecidenizhanyigit@gmail.com>
2025-03-07 17:08:46 -0600

    @Service annotations are set

--- src/main/java/com/Yu_Gi_Oh_App/duel_simulator/services/GameService.java
+++ src/main/java/com/Yu_Gi_Oh_App/duel_simulator/services/GameService.java
@@ -1,4 +1,7 @@
 package com.Yu_Gi_Oh_App.duel_simulator.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
 }
--- src/main/java/com/Yu_Gi_Oh_App/duel_simulator/services/PlayerService.java
+++ src/main/java/com/Yu_Gi_Oh_App/duel_simulator/services/PlayerService.java
@@ -1,4 +1,8 @@
 package com.Yu_Gi_Oh_App.duel_simulator.services;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerService {
 }
